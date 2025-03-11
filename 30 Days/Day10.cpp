54799f384add71584f941feedbe8d9617214802f
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-11 11:14:03 +0530

    day10

---
+++ 30 Days/Day10.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+bool isValid(string s) {
+    stack<char> st;
+    
+    for (char ch : s) {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            st.push(ch);
+        } else {
+            if (st.empty()) return false;
+            char top = st.top();
+            if ((ch == ')' && top == '(') || 
+                (ch == '}' && top == '{') || 
+                (ch == ']' && top == '[')) {
+                st.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    
+    return st.empty();
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << (isValid(s) ? "true" : "false") << endl;
+    return 0;
+}
